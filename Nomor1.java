484dea6fef5f061f825773a89545326ed160d1aa
Zahara <zaharaoyen123@gmail.com>
2024-12-12 09:19:44 +0700

    Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,24 +1,24 @@
 import java.util.Scanner;
-public class Nomor1 {
 
-public static void main(String[] args){
-Scanner input = new Scanner(System.in);
-        
-        /* Pekerjaan anda mulai dari sini */
-         System.out.print("Masukkan nilai n: ");
-        int n = input.nextInt();
-
-        tampilkanAngkaGanjil(n);
+public class no1 {
+    public static void tampilkanBilanganGanjil(int n) {
+        System.out.println("Bilangan ganjil dari 0 hingga " + n + " adalah:");
+        for (int i = 0; i <= n; i++) {
+            if (i % 2 != 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(); 
     }
 
-public static void tampilkanAngkaGanjil(int n) {
-        System.out.println("Angka ganjil dari 0 hingga " + n + ":");
-        for (int i = 1; i <= n; i += 2) {
-            System.out.print(i + " ");
-        }
-        /* Pekerjaan anda berakhir sini */
-
-        System.out.println(Nomor1.class.getDeclaredMethods().length); 
-    } 
-       
-        }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan bilangan n: ");
+        int n = input.nextInt();
+        
+        tampilkanBilanganGanjil(n);
+        
+        input.close();
+    }
+}
