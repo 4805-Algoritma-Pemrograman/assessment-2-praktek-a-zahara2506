0bae104d5963c8ecaafa1a0649fd116190b606a9
Zahara <zaharaoyen123@gmail.com>
2024-12-12 08:17:29 +0700

    Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,9 +4,19 @@ public class Nomor1 {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+        
         /* Pekerjaan anda mulai dari sini */
+         System.out.print("Masukkan nilai n: ");
+        int n = input.nextInt();
 
+        tampilkanAngkaGanjil(n);
+    }
+
+    public static void tampilkanAngkaGanjil(int n) {
+        System.out.println("Angka ganjil dari 0 hingga " + n + ":");
+        for (int i = 1; i <= n; i += 2) {
+            System.out.print(i + " ");
+        }
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
